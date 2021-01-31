23510445479d405388d9233f2a6e70de86a837ad
Ziggareto <benjamin.l.dayan@gmail.com>
2021-01-31 18:19:28 +0000

    Finish off posonly

--- ps1_attempt2/src/posonly/posonly2.py
+++ ps1_attempt2/src/posonly/posonly2.py
@@ -60,6 +60,21 @@ def main(train_path, valid_path, test_path, save_path):
 
     # Part (f): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to output_path_adjusted
+
+    # We use alpha = E[ h(x) | y = 1] for perfect predictor h(x) = p(y=1 | x) and assumed p(t=1 | x) in {0,1} 0
+    # uncertainty. For h we have a good estimate from logistic regression/ p(t=1 | x) = p(y=1 | x)/alpha
+
+    # empirical estimate, using sample average h(x_i) for x_i: y_i = 1
+    alpha_estimate = (1 / y_train.sum()) * clf.predict(x_train[y_train == 1]).sum()
+
+    # H(x) := h(x)/alpha is our prediction for p(t=1 | x). Then H(x) = 1/2 <=> h(x) = alpha/2 = 1/(1 + e^(-theta^T x))
+    # i.e. 2/alpha - 1 = e^(-theta^T x) so theta^T x = log(alpha/(2-alpha)
+    # Then H(x) = 1/2 <=> phi^T x = 0 where phi_0 = theta_0 - log(alpha/(2-alpha)
+    t_theta = clf.theta + np.array([-np.log(alpha_estimate / (2 - alpha_estimate)), 0., 0.])
+
+    util.plot(x_test, t_test, t_theta, os.path.splitext(output_path_adjusted)[0] + '_fig.png')
+    np.savetxt(output_path_adjusted, t_test_pred)
+    np.savetxt(output_path_adjusted + '_theta', clf.theta)
     # *** END CODER HERE
 
 if __name__ == '__main__':
